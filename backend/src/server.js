d80be4176646a7adb87d8b082556d1255674d361
matiasevillar <matias.villar@familysearch.org>
2025-10-21 08:47:30 -0600

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
+
 import authRoutes from './routes/auth.route.js'
 import messagesRoutes from './routes/message.route.js'
+
 dotenv.config()
 
 const app = express()
+const __dirname = path.resolve()
 
 const PORT = process.env.PORT || 3001
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messagesRoutes)
+
+// When the app is deployed to production, this code makes our Express backend also serve our React frontend.
+if (process.env.NODE_ENV === 'production') {
+  // serve static files
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`)
